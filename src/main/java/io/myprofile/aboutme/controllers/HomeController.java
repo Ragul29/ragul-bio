d2346344c6f759b709aaffad5fb3193aa1fb27fe
Ragul29 <rahulvenkat.1997@gmail.com>
2020-11-05 00:52:30 +0530

    Added Send message service

--- src/main/java/io/myprofile/aboutme/controllers/HomeController.java
+++ src/main/java/io/myprofile/aboutme/controllers/HomeController.java
@@ -1,15 +1,44 @@
 package io.myprofile.aboutme.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import io.myprofile.aboutme.model.Customer;
+import io.myprofile.aboutme.repository.CustomerRepository;
 
-@RestController
+
+@Controller
 public class HomeController {
+	private final CustomerRepository customerRepository;
 	
+	@Autowired
+	public HomeController(CustomerRepository customerRepository) {
+		
+		this.customerRepository = customerRepository;
+	}
+	
+	
+
+	@PostMapping("/user/query")
+	    public String addStudent(@Validated Customer customer, BindingResult result, Model model) {
+	        if (result.hasErrors()) {
+	            return "landing";
+	        }
+
+	        customerRepository.save(customer);
+	        return "redirect:/";
+	    }
+	
+	
+
 	@RequestMapping("/")
 	public ModelAndView Home()
 	{
