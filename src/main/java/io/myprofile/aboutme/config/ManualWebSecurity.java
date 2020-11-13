93007a7927b3009aed49a18ce65c71da7ea41354
Ragul29 <rahulvenkat.1997@gmail.com>
2020-11-14 00:28:31 +0530

    Changes removed

--- src/main/java/io/myprofile/aboutme/config/ManualWebSecurity.java
+++ src/main/java/io/myprofile/aboutme/config/ManualWebSecurity.java
@@ -1,15 +1,8 @@
 package io.myprofile.aboutme.config;
 
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Configuration;
 
-@EnableWebSecurity
-public class ManualWebSecurity extends
-WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.cors().and().csrf().disable();
-	}
+
+public class ManualWebSecurity {
 }
